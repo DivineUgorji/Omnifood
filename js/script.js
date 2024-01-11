afb3d8e8d6e1be97a095258b42acbf5171a4e8b7
DivineUgorji <divineugorji37@gmail.com>
2024-01-11 15:16:16 +0100

    Implement mobile Navigation and Smooth Scroll

--- js/script.js
+++ js/script.js
@@ -5,14 +5,61 @@ yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
-
 const btnNav = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 btnNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
 ///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
+// Smooth scrolling animations
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    //   if (link.classList.contains("main-nav-link"))
+    //     headerEl.classList.toggle("nav-open");
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// const allLinks = document.querySelectorAll("a:link");
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+//     // console.log(href);
+
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behaviour: "smooth",
+//       });
+
+//     if (href !== "#" && href.startsWith("#")) console.log(href);
+//   });
+// });
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
